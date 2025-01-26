4e97257040c956890561e6826ebd6cb127d279db
Aliaksandr Famchankou <afamchankou@gmail.com>
2025-01-26 11:52:35 +0100

    feat: init fe app

---
+++ client/src/reportWebVitals.ts
@@ -0,0 +1,16 @@
+import { LCPMetric, CLSMetric, FCPMetric, TTFBMetric } from "web-vitals";
+
+type Metric = LCPMetric | CLSMetric | FCPMetric | TTFBMetric;
+
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import("web-vitals").then(({ onCLS, onFCP, onLCP, onTTFB }) => {
+      onCLS(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
+    });
+  }
+};
+
+export default reportWebVitals;
